eed914df13eadf9429f20b711516e414d869b793
Sardar <sardorbeksattarov4@gmail.com>
2026-06-30 02:06:00 +0600

    Добавиль функц - about

--- main.py
+++ main.py
@@ -13,14 +13,13 @@ dp = Dispatcher()
 async def cmd_starts(message: Message):
     await message.answer(f'Hello , {message.from_user.first_name} я твой первый бот ')
 
-@dp.message(Command("info"))
+@dp.message(Command("about"))
 async def bot_info(message: Message):
     await message.answer('Инфо о Боте\n'
                          'Версия Бота 1.0\n'
                          'Разработчик(Сардар)\n'
+                         'Пока бот ничего не умеет делать, кроме информации о себе\n'
                          )
-
-
 
 async def main():
     await dp.start_polling(bot)
