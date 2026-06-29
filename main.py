8c86b46fe64ffc4881bf804ab9eee08eda76e1e0
Sardar <sardorbeksattarov4@gmail.com>
2026-06-30 01:19:13 +0600

    1-th, Project

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio 
+
+from aiogram import Bot, Dispatcher 
+from aiogram.types import Message
+from aiogram.filters import CommandStart, Command 
+ 
+Bot_Token = "8940390700:AAH0mYwT4VUD05sTFR0m2R3_o628RPuQbHo"
+
+bot = Bot(token = Bot_Token)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_starts(message: Message):
+    await message.answer(f'Hello , {message.from_user.first_name} я твой первый бот ')
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
